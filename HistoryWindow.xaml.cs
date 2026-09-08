851932e18f6f99797317cf88a039af9c0435863d
frankqwang <20528833+frankqwang@users.noreply.github.com>
2026-09-08 17:13:55 +0800

    Unify history window visual language

--- HistoryWindow.xaml.cs
+++ HistoryWindow.xaml.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using DoneBubble.Models;
 using DoneBubble.ViewModels;
 namespace DoneBubble;
@@ -9,4 +11,7 @@ public partial class HistoryWindow : Window
     public HistoryWindow(MainViewModel model) { InitializeComponent(); this.model = model; DataContext = model; }
     private void Delete_Click(object sender, RoutedEventArgs e) { if (((Button)sender).Tag is RecordItem item) model.Delete(item); }
     private void Refresh_Click(object sender, RoutedEventArgs e) => model.Refresh();
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
+    private void Header_Drag(object sender, MouseButtonEventArgs e) { if (e.LeftButton == MouseButtonState.Pressed) { try { DragMove(); } catch (InvalidOperationException) { } } }
+    private void Window_KeyDown(object sender, KeyEventArgs e) { if (e.Key == Key.Escape) { Close(); e.Handled = true; } }
 }
